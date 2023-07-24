7dff524f2896c8d9ac17564127ec468b8031112d
이원주 <wjleedev>
2023-07-24 12:13:04 +0900

    add

--- ntrus/src/index.tsx
+++ ntrus/src/index.tsx
@@ -8,13 +8,9 @@ const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-
-// {/*   <React.StrictMode> */}
-// {/*   <BrowserRouter> */}
+<React.StrictMode>
     <App />
-//   </BrowserRouter>
-
-//   </React.StrictMode>
+    </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
